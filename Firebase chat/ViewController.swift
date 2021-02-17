ccf171df079a7c0f3d64edb0f2f77da0e582948d
Vit 42 <v.v.safronov@gmail.com>
2021-02-17 11:38:52 +0100

    Initial Commit

---
+++ Firebase chat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Firebase chat
+//
+//  Created by Vitalii Safronov on 2021. 02. 17..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
